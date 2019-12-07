2d3e0991d9ba31105de7838b1e366cef62d021df
Eric murray <ericwmurray1994@gmail.com>
2019-12-07 00:21:44 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,6 @@
 def meal_choice(broccoli, macaroni, protien = "meat")
   puts "What a delicious meal!"
+  "What a delicious meal!"
   puts "A plate of #{protien} with #{broccoli} and #{macaroni}."
 end
 
